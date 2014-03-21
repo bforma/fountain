7d33992da9e4b5dcfd92245ec9a658626e6c80c5
Ian Unruh <ianunruh@gmail.com>
2014-03-21 01:56:23 -0500

    Changed Logger used in tests
    Now using real Logger class that outputs to a `StringIO` object

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,10 @@
 require 'simplecov'
+require 'stringio'
 
 SimpleCov.start
 
 require 'fountain'
 
-class NullLogger
-  def method_missing(*)
-    # Just ignore any logging calls
-  end
-end
-
 Fountain.configure do |config|
-  config.logger = NullLogger.new
+  config.logger = Logger.new(StringIO.new)
 end
